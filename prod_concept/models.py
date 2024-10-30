d129fc22b75d3258a814fcc9c76dfa6c309dce61
Ron Smith <ron.smith37@gmail.com>
2024-10-31 05:34:43 +1000

    Added blockAdjecency model

--- prod_concept/models.py
+++ prod_concept/models.py
@@ -15,3 +15,21 @@ class FlowModelConceptRing(Location):
 
     def __str__(self):
         return self.blastsolids_id
+
+
+class BlockAdjacency(models.Model):
+    block = models.ForeignKey(FlowModelConceptRing, on_delete=models.CASCADE, related_name='adjacent_blocks')
+    adjacent_block = models.ForeignKey(FlowModelConceptRing, on_delete=models.CASCADE, related_name='adjacent_to')
+    direction = models.CharField(max_length=2, choices=[
+        ('N', 'North'),
+        ('S', 'South'),
+        ('E', 'East'),
+        ('W', 'West'),
+        ('NE', 'Northeast'),
+        ('NW', 'Northwest'),
+        ('SE', 'Southeast'),
+        ('SW', 'Southwest')
+    ])
+
+    def __str__(self):
+        return f"Block {self.block.blastsolids_id} adjacent to {self.adjacent_block.blastsolids_id} ({self.direction})"
