4dc1fbf1aceba826e877fa4dbf6c265c8bce53be
Ron Smith <ron.smith37@gmail.com>
2025-03-05 20:33:12 +1000

    Add firing group model to admin

--- prod_actual/admin.py
+++ prod_actual/admin.py
@@ -16,5 +16,30 @@ class BoggedTonnesAdmin(admin.ModelAdmin):
     get_production_ring_alias.short_description = 'Production Ring Alias'
 
 
+class MultifireGroupAdmin(admin.ModelAdmin):
+    list_display = ('multifire_group_id', 'name', 'level', 'is_active',
+                    'total_tonnage', 'total_volume', 'created_at', 'entered_by', 'deactivated_by')
+    list_filter = ('is_active', 'level', 'created_at')
+    search_fields = ('name', 'entered_by__username',
+                     'deactivated_by__username')
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        ('General Info', {
+            'fields': ('name', 'level', 'is_active')
+        }),
+        ('Statistics', {
+            'fields': ('total_volume', 'total_tonnage', 'avg_density', 'avg_au', 'avg_cu')
+        }),
+        ('Rings Data', {
+            'fields': ('pooled_rings', 'group_rings')
+        }),
+        ('Timestamps & Users', {
+            'fields': ('created_at', 'updated_at', 'entered_by', 'deactivated_by')
+        }),
+    )
+
+
 admin.site.register(m.ProductionRing, ProdActualAdmin)
 admin.site.register(m.BoggedTonnes, BoggedTonnesAdmin)
+admin.site.register(m.MultifireGroup, MultifireGroupAdmin)
