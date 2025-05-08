0cd5aefca9e8cb9ff2da0258340ac664f3c0d51b
Ron Smith <ron.smith37@gmail.com>
2025-05-08 15:02:44 +1000

    Remove development from roadmap

--- dev_concept/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-import dev_concept.api.views as v
-
-
-urlpatterns = [
-    path('', v.EmptyView.as_view(), name='empty-view'),
-]
--- dev_concept/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DevConceptConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dev_concept'
